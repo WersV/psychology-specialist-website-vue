c58599b959b9c1529eafc746fb55f764e1e76868
Przemysław Botwina <przemek.botwina@o2.pl>
2022-11-21 17:37:15 +0100

    create hamburger menu and hello page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
+<template>
+  <MenuAndHello />
+</template>
+
 <script>
+import MenuAndHello from "./views/MenuAndHello.vue";
+
 export default {
-  data() {},
+  data() {
+    return {
+      name: "App",
+      components: {
+        MenuAndHello,
+      },
+    };
+  },
+  components: { MenuAndHello },
 };
 </script>
 
-<template>
-  <p>asd</p>
-</template>
-
-<style scoped></style>
+<style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: arial;
+  /* overflow: hidden; */
+  #app.active {
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+}
+</style>
